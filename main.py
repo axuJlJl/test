60cb8f87001c2514897aec1dc05230deebe8df88
Eremeev D <113033230+axuJlJl@users.noreply.github.com>
2022-10-11 14:13:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-def Bar():
-    print("Bar!")
-def foo():
-    print("Foo!")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')

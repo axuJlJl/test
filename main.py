82f42fba252bc67f438ec53f964de80da9fcc5fd
Denis <ahu_ll@mail.ru>
2022-10-11 13:50:59 +0300

    Add new func

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def foo():
+    print("Foo!")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
